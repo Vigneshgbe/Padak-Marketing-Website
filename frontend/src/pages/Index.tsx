8ab8dbcd0eb2959df80e2c58d7e578cfd85498eb
Sweety-Vigneshg <kuttydravid463@gmail.com>
2025-07-31 18:54:01 +0530

    correction

--- frontend/src/pages/Index.tsx
+++ frontend/src/pages/Index.tsx
@@ -1,7 +1,6 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Services } from "@/components/Services";
-import { About } from "@/pages/About";
 import { Contact } from "@/components/Contact";
 import { Footer } from "@/components/Footer";
 
