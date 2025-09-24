58426dca0e030c22553f376757a90f3356fbed84
Vignesh G <kuttydravid463@gmail.com>
2025-09-24 13:30:20 +0530

    code update removed timeout

--- backend/server.js
+++ backend/server.js
@@ -32,10 +32,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER, // || 'root',
   password: process.env.DB_PASSWORD, // || '',
   database: process.env.DB_NAME, // || 'padak_digital_marketing',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  connectTimeout: 60000,
+  //waitForConnections: true,
+  //connectionLimit: 10,
+  //queueLimit: 0,
+  //connectTimeout: 60000,
 });
 
 // Test database connection
